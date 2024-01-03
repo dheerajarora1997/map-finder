30bb7d22fabd6b207cc1f81169b0a735f2481e93
Dheeraj Arora <dheeraj.arora@comviva.com>
2024-01-03 21:18:50 +0530

    changes for auth 0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,21 @@ import Header from "./Components/Header";
 import store from "../store";
 import Login from "./Components/LoginForm";
 import Main from "./main";
-import { Auth0Provider } from '@auth0/auth0-react';
+import { Auth0Provider } from "@auth0/auth0-react";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function Home() {
-  const url = window.location.origin;
+  useEffect(() => {
+    const url = window.location.origin;
+  }, []);
   return (
     <Auth0Provider
       domain="india-999.us.auth0.com"
       clientId="6HVppH4Y9WCyZOc2VlTD4EIsoBn4Abf4"
       authorizationParams={{
-        redirect_uri: url,
+        redirect_uri:
+          typeof window !== "undefined" ? window.location.origin : "",
       }}
     >
       <Provider store={store}>
